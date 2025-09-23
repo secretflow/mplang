215b3ee0c480915115948f794392d2cf853aa3d9
tongke <124763920+tongke6@users.noreply.github.com>
2025-09-23 19:16:28 +0800

    Fix run-tutorials pipeline not exit on error issue (#158)

--- tutorials/run.sh
+++ tutorials/run.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+#
 # Copyright 2025 Ant Group Co., Ltd.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,6 +13,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+set -e
 
 uv run python tutorials/0_basic.py
 uv run python tutorials/1_condition.py

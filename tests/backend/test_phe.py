3ee9eaf0e1ebf426ac8758176455042556cc80d7
Jin Tan <tanjin.tj@antgroup.com>
2025-09-23 07:44:10 +0000

    fix license

--- tests/backend/test_phe.py
+++ tests/backend/test_phe.py
@@ -1,9 +1,22 @@
-# ruff: noqa: I001
+# Copyright 2025 Ant Group Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import numpy as np
 import pytest
 
 from mplang.backend.base import create_runtime, list_registered_kernels
-from mplang.backend.phe import CipherText, PublicKey, PrivateKey
+from mplang.backend.phe import CipherText, PrivateKey, PublicKey
 from mplang.core.dtype import BOOL, INT32
 from mplang.core.pfunc import PFunction
 from mplang.core.tensor import TensorType

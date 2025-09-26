28248458fd2d4de2021647145d313ef0b6bf78be
lianhua <zhoujinjin.zjj@antgroup.com>
2025-09-26 02:24:41 +0000

    reformat

--- examples/xgboost/sgb.py
+++ examples/xgboost/sgb.py
@@ -920,9 +920,7 @@ def build_tree(
             # bt_level: (m,), with values in (0,1,2,...,n_nodes_level-1)
             cur_pp_subgroup_map = simp.runAt(
                 cur_pp_rank, partial(_get_subgroup_map, group_size=n_nodes_level)
-            )(
-                bt_levels
-            )  # (n_nodes_level, m)
+            )(bt_levels)  # (n_nodes_level, m)
 
             # 1.2.2 pp encrypt the accumulated histogram.
             cur_pp_enc_hist_cumsum: list[MPObject] = (
@@ -936,9 +934,9 @@ def build_tree(
                 )
             )
 
-            assert (
-                len(cur_pp_enc_hist_cumsum) == n_nodes_level
-            ), f"Expect {n_nodes_level} outputs, got {len(cur_pp_enc_hist_cumsum)}"
+            assert len(cur_pp_enc_hist_cumsum) == n_nodes_level, (
+                f"Expect {n_nodes_level} outputs, got {len(cur_pp_enc_hist_cumsum)}"
+            )
 
             # 1.2.3 pp send the encrypted histogram to ap.
             cur_pp_enc_hist_cumsum: list[MPObject] = p2p_list(
@@ -968,9 +966,7 @@ def build_tree(
                     gamma=gamma,
                     min_child_weight=min_child_weight,
                 ),
-            )(
-                cur_pp_dec_hist_cumsum
-            )
+            )(cur_pp_dec_hist_cumsum)
             cur_level_best_gains.append(cur_pp_best_gains)
             cur_level_best_features.append(cur_pp_best_features)
             cur_level_best_threshold_idxs.append(cur_pp_best_threshold_idxs)
@@ -1514,9 +1510,9 @@ class SecureBoost:
 
         # TODO: support more general party ids
         assert self.active_party_id == 0, "Only active party id 0 is supported now"
-        assert self.passive_party_ids == list(
-            range(1, len(passive_party_ids) + 1)
-        ), f"Only passive party ids {list(range(1, len(passive_party_ids) + 1))} are supported now"
+        assert self.passive_party_ids == list(range(1, len(passive_party_ids) + 1)), (
+            f"Only passive party ids {list(range(1, len(passive_party_ids) + 1))} are supported now"
+        )
 
         self.trees: TreeEnsemble | None = None
 
@@ -1531,9 +1527,9 @@ class SecureBoost:
             y_data: the label vector of the active party
         """
         self._check_all_datas(all_datas)
-        assert (
-            y_data.pmask == self.active_party_mask
-        ), f"y_data.pmask: {y_data.pmask}, self.active_party_mask: {self.active_party_mask}"
+        assert y_data.pmask == self.active_party_mask, (
+            f"y_data.pmask: {y_data.pmask}, self.active_party_mask: {self.active_party_mask}"
+        )
 
         # 1. do the binning.
         # TODO: support more sophisticated binning schemes.
@@ -1628,9 +1624,9 @@ class SecureBoost:
             assert pp_data.pmask == self.passive_party_masks[i]
         # check whether ap_data and pp_datas have the same number of rows
         for pp_data in pp_datas:
-            assert (
-                pp_data.shape[0] == ap_data.shape[0]
-            ), "The number of rows of ap_data and pp_datas must be the same"
+            assert pp_data.shape[0] == ap_data.shape[0], (
+                "The number of rows of ap_data and pp_datas must be the same"
+            )
 
     # debug only
     def predict_leaves(self, all_datas: list[MPObject]) -> MPObject:

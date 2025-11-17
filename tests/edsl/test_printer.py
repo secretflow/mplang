de102608d2d65cfe7082f4ed932ca42bac151621
Jin Tan <tanjin.tj@antgroup.com>
2025-11-17 11:19:23 +0000

    refactor: replace custom add_constant helper with direct graph operations in tests

--- tests/edsl/test_printer.py
+++ tests/edsl/test_printer.py
@@ -16,29 +16,15 @@
 
 from textwrap import dedent
 
-from mplang2.edsl.graph import Graph, Operation
+from mplang2.edsl.graph import Graph
 from mplang2.edsl.printer import GraphPrinter, format_graph
 from mplang2.edsl.typing import Tensor, f32, i32
-
-
-def _add_constant(graph: Graph, value, type):
-    """Helper to add constant to graph (for testing only)."""
-    out_val = graph.add_value(type)
-    op = Operation(
-        opcode="constant",
-        inputs=[],
-        outputs=[out_val],
-        attrs={"value": value},
-    )
-    out_val.defining_op = op
-    graph.operations.append(op)
-    return out_val
 
 
 def _build_simple_graph() -> Graph:
     g = Graph()
     x = g.add_input("x", Tensor[f32, (1,)])
-    const = _add_constant(g, 1.0, f32)
+    const = g.add_op("constant", [], output_types=[f32], attrs={"value": 1.0})[0]
     [y] = g.add_op("add", [x, const], output_types=[Tensor[f32, (1,)]])
     g.add_output(y)
     return g
@@ -74,7 +60,9 @@ def test_graph_printer_with_regions():
 
     else_graph = Graph()
     else_x = else_graph.add_input("x", Tensor[f32, ()])
-    zero = _add_constant(else_graph, 0.0, f32)
+    zero = else_graph.add_op("constant", [], output_types=[f32], attrs={"value": 0.0})[
+        0
+    ]
     [else_out] = else_graph.add_op(
         "add", [else_x, zero], output_types=[Tensor[f32, ()]]
     )
